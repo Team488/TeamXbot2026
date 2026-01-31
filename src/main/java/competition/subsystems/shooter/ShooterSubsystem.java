e4039562bc975ec2e9e04992b2a3030dd1318f73
Clulogh <197954988+Clulogh@users.noreply.github.com>
2026-01-31 14:52:53 -0800

    Made ShooterMaintainerCommand

--- src/main/java/competition/subsystems/shooter/ShooterSubsystem.java
+++ src/main/java/competition/subsystems/shooter/ShooterSubsystem.java
@@ -1,29 +1,38 @@
 package competition.subsystems.shooter;
 
 import competition.electrical_contract.ElectricalContract;
+import edu.wpi.first.units.AngularAccelerationUnit;
+import edu.wpi.first.units.measure.AngularVelocity;
+import xbot.common.command.BaseSetpointSubsystem;
 import xbot.common.command.BaseSubsystem;
 import xbot.common.controls.actuators.XCANMotorController;
 import xbot.common.controls.actuators.XCANMotorControllerPIDProperties;
+import xbot.common.controls.actuators.XSpeedController;
 import xbot.common.properties.DoubleProperty;
 import xbot.common.properties.PropertyFactory;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
+import java.lang.annotation.Target;
+
 import static edu.wpi.first.units.Units.RPM;
 
 @Singleton
-public class ShooterSubsystem extends BaseSubsystem {
+public class ShooterSubsystem extends BaseSetpointSubsystem<AngularVelocity, Double> {
     public final XCANMotorController shooterMotor;
-
+    public ElectricalContract electricalContract;
     public DoubleProperty targetVelocity;
+    double rotationAtZero = 0;
 
     @Inject
     public ShooterSubsystem(XCANMotorController.XCANMotorControllerFactory xcanMotorControllerFactory,
-                            ElectricalContract eletricalContract, PropertyFactory propertyFactory) {
+                            ElectricalContract electricalContract, PropertyFactory propertyFactory) {
         propertyFactory.setPrefix(this);
-        if (eletricalContract.isShooterReady()) {
-            shooterMotor = xcanMotorControllerFactory.create(eletricalContract.getShooterMotor(),
+        this.electricalContract = electricalContract;
+
+        if (electricalContract.isShooterReady()) {
+            shooterMotor = xcanMotorControllerFactory.create(electricalContract.getShooterMotor(),
                     getPrefix(),"ShooterMotorPID",
                     new XCANMotorControllerPIDProperties(
                             0.1,
@@ -70,5 +79,39 @@ public class ShooterSubsystem extends BaseSubsystem {
         if (shooterMotor != null) {
             shooterMotor.periodic();
         }
+    }
+
+    @Override
+    public AngularVelocity getCurrentValue() {
+        if (electricalContract.isShooterReady()) {
+            return shooterMotor.getVelocity();
+        }
+        return RPM.zero(); //rpm = rotation per minute
+    }
+
+    @Override
+    public AngularVelocity getTargetValue() {
+        return RPM.of(targetVelocity.get());
+        //targetVelocity = double property - .get() makes it into a double
+    }
+
+    @Override
+    public void setTargetValue(AngularVelocity value) {
+
+    }
+
+    @Override
+    public void setPower(Double power) {
+
+    }
+
+    @Override
+    public boolean isCalibrated() {
+        return false;
+    }
+
+    @Override
+    protected boolean areTwoTargetsEquivalent(AngularVelocity target1, AngularVelocity target2) {
+        return false;
     }
 }
---
+++ src/main/java/competition/subsystems/shooter/commands/ShooterWheelMaintainerCommand.java
@@ -0,0 +1,91 @@
+package competition.subsystems.shooter.commands;
+
+import competition.subsystems.shooter.ShooterSubsystem;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.units.Units;
+import edu.wpi.first.units.measure.AngularVelocity;
+import xbot.common.command.BaseMaintainerCommand;
+import xbot.common.logic.HumanVsMachineDecider;
+import xbot.common.math.PID;
+import xbot.common.math.PIDManager;
+import xbot.common.math.PIDManager_Factory;
+import xbot.common.properties.DoubleProperty;
+import xbot.common.properties.PropertyFactory;
+
+import javax.inject.Inject;
+
+public class ShooterWheelMaintainerCommand extends BaseMaintainerCommand<AngularVelocity, Double> {
+
+    final ShooterSubsystem shooterWheel;
+    final DoubleProperty humanMaxPower;
+    final DoubleProperty humanMinPower;
+    final PIDManager pidManager;
+
+    @Inject
+    public ShooterWheelMaintainerCommand(ShooterSubsystem shooterWheel, PropertyFactory pf,
+                                         HumanVsMachineDecider.HumanVsMachineDeciderFactory hvmFactory,
+                                         PIDManager.PIDManagerFactory pidManagerFactory) {
+        super(shooterWheel, pf, hvmFactory, 0.01, 0.01); // tweak number
+        pf.setPrefix(this);
+        this.shooterWheel = shooterWheel;
+        this.pidManager = pidManagerFactory.create(
+                pf.getPrefix() + "/ShooterWheelMaintainerPID",
+                0,
+                0,
+                0,
+                0,
+                0,
+                0
+        );
+        humanMaxPower = pf.createPersistentProperty("ShooterMaxPower", 0.2); // tweak number
+        humanMinPower = pf.createPersistentProperty("ShooterMinPower", -0.2); // tweak number
+
+    }
+
+    @Override
+    public void initialize() {
+        super.initialize();
+    }
+
+    @Override
+    protected void coastAction() {
+        shooterWheel.stop();
+    }
+
+    @Override
+    protected void calibratedMachineControlAction() {
+        //use pid to reach target
+        double error = getErrorMagnitude();
+        double power = pidManager.calculate(
+                shooterWheel.getCurrentValue().in(Units.RPM),
+                shooterWheel.getTargetValue().in(Units.RPM)
+        );
+
+        double max = humanMaxPower.get();
+        double min = humanMinPower.get();
+        shooterWheel.setPower(power);
+        //dont really need calibrate because it will just continue rotating.
+    }
+
+    @Override
+    protected double getErrorMagnitude() {
+        var current = shooterWheel.getCurrentValue();
+        var target = shooterWheel.getTargetValue();
+        var error = target.minus(current);
+        return error.in(Units.RPM);
+    }
+
+    @Override
+    protected Double getHumanInput() {
+        return 0.0;
+    }
+    // pushing a button and inputs saying that oh someone is moving
+    // the forwards button and how much they are moving
+
+
+@Override
+protected double getHumanInputMagnitude() {
+    return Math.abs(getHumanInput());
+    // magnitude = distance/absolute value
+    }
+}
