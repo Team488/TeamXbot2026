80aa4356c8f8b3cbaf2a70b53d91799c99681c34
Alex Schokking <aschokking@gmail.com>
2026-01-31 14:48:45 -0800

    Add 2nd slot for PID properties

--- src/main/java/competition/subsystems/shooter/ShooterSubsystem.java
+++ src/main/java/competition/subsystems/shooter/ShooterSubsystem.java
@@ -4,6 +4,9 @@ import competition.electrical_contract.ElectricalContract;
 import xbot.common.command.BaseSubsystem;
 import xbot.common.controls.actuators.XCANMotorController;
 import xbot.common.controls.actuators.XCANMotorControllerPIDProperties;
+import xbot.common.controls.actuators.XCANMotorController.MotorPidMode;
+import xbot.common.controls.sensors.XDigitalInput;
+import xbot.common.injection.electrical_contract.DeviceInfo;
 import xbot.common.properties.DoubleProperty;
 import xbot.common.properties.PropertyFactory;
 
@@ -17,15 +20,20 @@ public class ShooterSubsystem extends BaseSubsystem {
     public final XCANMotorController leftShooterMotor;
     public final XCANMotorController middleShooterMotor;
     public final XCANMotorController rightShooterMotor;
+    public final XDigitalInput shooterBeamBreak;
 
-    public DoubleProperty targetVelocity;
+    public final DoubleProperty targetVelocity;
+    public final DoubleProperty activeShotFeedForward;
 
     @Inject
     public ShooterSubsystem(XCANMotorController.XCANMotorControllerFactory xcanMotorControllerFactory,
                             ElectricalContract eletricalContract,
-                            PropertyFactory propertyFactory) {
+                            PropertyFactory propertyFactory, XDigitalInput.XDigitalInputFactory digitalInputFactory) {
 
         propertyFactory.setPrefix(this);
+        // TODO: Put in contract
+        shooterBeamBreak = digitalInputFactory.create(new DeviceInfo("BeamBreak", 1), this.getPrefix());
+        this.dataFrameRefreshables.add(shooterBeamBreak);
 
         var defaultPIDProperties = new XCANMotorControllerPIDProperties(
                 0.1,
@@ -36,9 +44,14 @@ public class ShooterSubsystem extends BaseSubsystem {
                 1,
                 0);
 
+        
+        
+        this.activeShotFeedForward = propertyFactory.createPersistentProperty("Active Shot Feed Forward", 0.0);
+        
         if (eletricalContract.isLeftShooterReady()) {
             this.leftShooterMotor = xcanMotorControllerFactory.create(eletricalContract.getLeftShooterMotor(),
                     getPrefix(), "ShooterMotor", defaultPIDProperties);
+            this.leftShooterMotor.setPidDirectly(0, 0, 0, 0, 0, 1);
             this.registerDataFrameRefreshable(leftShooterMotor);
         } else {
             this.leftShooterMotor = null;
@@ -92,7 +105,11 @@ public class ShooterSubsystem extends BaseSubsystem {
 
     public void runAtTargetVelocity() {
         if (leftShooterMotor != null) {
-            leftShooterMotor.setVelocityTarget(RPM.of(targetVelocity.get()));
+            if(shooterBeamBreak.get()) {
+                leftShooterMotor.setVelocityTarget(RPM.of(targetVelocity.get()), MotorPidMode.DutyCycle, 1);
+            } else {
+                leftShooterMotor.setVelocityTarget(RPM.of(targetVelocity.get()));
+            }
         }
 
         if (middleShooterMotor != null) {
@@ -107,17 +124,16 @@ public class ShooterSubsystem extends BaseSubsystem {
     public void periodic() {
         if (leftShooterMotor != null) {
             leftShooterMotor.periodic();
-            leftShooterMotor.setVelocityTarget(RPM.of(targetVelocity.get()));
+            // Manually sync the slot 1 pid properties
+            leftShooterMotor.setPidDirectly(0, 0, 0, activeShotFeedForward.get(), 0, 1);
         }
 
         if (middleShooterMotor != null) {
             middleShooterMotor.periodic();
-            middleShooterMotor.setVelocityTarget(RPM.of(targetVelocity.get()));
         }
 
         if (rightShooterMotor != null) {
             rightShooterMotor.periodic();
-            rightShooterMotor.setVelocityTarget(RPM.of(targetVelocity.get()));
         }
     }
 }
