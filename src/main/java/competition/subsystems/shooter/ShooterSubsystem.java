85138d7dab767c095a23237b3e0b4d14799906bc
Alex Schokking <aschokking@gmail.com>
2026-02-03 19:55:24 -0800

    Dont turn on pid in periodic, bad

--- src/main/java/competition/subsystems/shooter/ShooterSubsystem.java
+++ src/main/java/competition/subsystems/shooter/ShooterSubsystem.java
@@ -107,17 +107,14 @@ public class ShooterSubsystem extends BaseSubsystem {
     public void periodic() {
         if (leftShooterMotor != null) {
             leftShooterMotor.periodic();
-            leftShooterMotor.setVelocityTarget(RPM.of(targetVelocity.get()));
         }
 
         if (middleShooterMotor != null) {
             middleShooterMotor.periodic();
-            middleShooterMotor.setVelocityTarget(RPM.of(targetVelocity.get()));
         }
 
         if (rightShooterMotor != null) {
             rightShooterMotor.periodic();
-            rightShooterMotor.setVelocityTarget(RPM.of(targetVelocity.get()));
         }
     }
 }
