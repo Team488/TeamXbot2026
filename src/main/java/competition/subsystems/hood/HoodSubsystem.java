6581c0512e1279d7ebc61e41e2e850e72972341d
AlanYuan99 <alanyuan709@gmail.com>
2026-02-03 20:48:45 -0800

    most of setpoint subsystem is done

--- src/main/java/competition/subsystems/hood/HoodSubsystem.java
+++ src/main/java/competition/subsystems/hood/HoodSubsystem.java
@@ -2,26 +2,36 @@ package competition.subsystems.hood;
 
 import competition.electrical_contract.ElectricalContract;
 
+import edu.wpi.first.units.measure.Angle;
+import xbot.common.command.BaseSetpointSubsystem;
 import xbot.common.command.BaseSubsystem;
 import xbot.common.controls.actuators.XCANMotorController;
 import xbot.common.controls.actuators.XCANMotorControllerPIDProperties;
 import xbot.common.properties.DoubleProperty;
 import xbot.common.properties.PropertyFactory;
 
+import static edu.wpi.first.units.Units.Degrees;
+import static edu.wpi.first.units.Units.Rotations;
+
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
 @Singleton
-public class HoodSubsystem extends BaseSubsystem {
+public class HoodSubsystem extends BaseSetpointSubsystem<Angle, Double> {
     public final XCANMotorController hoodMotor;
+    public ElectricalContract electricalContract;
 
+    private Angle targetAngle = Rotations.of(0);
+    private boolean isCalibrated = false;
     public DoubleProperty openPower;
     public DoubleProperty closePower;
+
     @Inject
     public HoodSubsystem(XCANMotorController.XCANMotorControllerFactory xcanMotorControllerFactory,
                          ElectricalContract electricalContract, PropertyFactory propertyFactory) {
 
         propertyFactory.setPrefix(this);
+        this.electricalContract = electricalContract;
 
         if (electricalContract.isHoodReady()) {
             this.hoodMotor = xcanMotorControllerFactory.create(electricalContract.getHoodMotor(),
@@ -52,4 +62,38 @@ public class HoodSubsystem extends BaseSubsystem {
         }
     }
 
+    @Override
+    public Angle getCurrentValue() {
+        if (electricalContract.isHoodReady()){
+            return Degrees.of(hoodMotor.getPosition().in(Rotations));
+        }
+        return Degrees.of(0);
+    }
+
+    @Override
+    public Angle getTargetValue() {
+        return targetAngle;
+    }
+
+    @Override
+    public void setTargetValue(Angle value) {
+        targetAngle = value;
+    }
+
+    @Override
+    public void setPower(Double power) {
+        if (electricalContract.isHoodReady()) {
+            hoodMotor.setPower(power);
+        }
+    }
+
+    @Override
+    public boolean isCalibrated() {
+        return isCalibrated;
+    }
+
+    @Override
+    protected boolean areTwoTargetsEquivalent(Angle target1, Angle target2) {
+        return target1.isEquivalent(target2);
+    }
 }
