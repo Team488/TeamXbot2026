759065cd0ee7b085a01cf4faebbed9155b641bd3
WeWaWu <wwu5528@gmail.com>
2026-02-05 19:25:23 -0800

    fixes

--- src/main/java/competition/electrical_contract/RoboxContract.java
+++ src/main/java/competition/electrical_contract/RoboxContract.java
@@ -4,6 +4,7 @@ import xbot.common.injection.electrical_contract.CANBusId;
 import xbot.common.injection.electrical_contract.CANMotorControllerInfo;
 import xbot.common.injection.electrical_contract.CANMotorControllerOutputConfig;
 import xbot.common.injection.electrical_contract.MotorControllerType;
+import xbot.common.injection.electrical_contract.PDHPort;
 import xbot.common.injection.electrical_contract.SparkMaxMotorControllerOutputConfig;
 
 import javax.inject.Inject;
@@ -21,15 +22,39 @@ public class RoboxContract extends Contract2026 {
     public boolean areCanCodersReady() { return false; }
 
     @Override
-    public boolean isShooterReady() { return true; }
+    public boolean isLeftShooterReady() { return false; }
 
     @Override
-    public CANMotorControllerInfo getShooterMotor() {
+    public boolean isMiddleShooterReady() { return false; }
+
+    @Override
+    public boolean isRightShooterReady() { return false; }
+
+    @Override
+    public CANMotorControllerInfo getLeftShooterMotor() {
         return new CANMotorControllerInfo("ShooterMotor",
-                MotorControllerType.SparkMax,
+                MotorControllerType.TalonFx,
                 CANBusId.RIO,
-                32,
-                new SparkMaxMotorControllerOutputConfig()
-                        .withSmartCurrentLimit(Amps.of(80)));
+                918,
+                PDHPort.PDH00,
+                new CANMotorControllerOutputConfig());
+    }
+
+    @Override
+    public CANMotorControllerInfo getMiddleShooterMotor() {
+        return new CANMotorControllerInfo("ShooterMotor",
+                MotorControllerType.TalonFx,
+                CANBusId.RIO,
+                211,
+                new CANMotorControllerOutputConfig());
+    }
+
+    @Override
+    public CANMotorControllerInfo getRightShooterMotor() {
+        return new CANMotorControllerInfo("ShooterMotor",
+                MotorControllerType.TalonFx,
+                CANBusId.RIO,
+                900,
+                new CANMotorControllerOutputConfig());
     }
 }
