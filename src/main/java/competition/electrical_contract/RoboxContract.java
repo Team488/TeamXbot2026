f78ff835b3c41421efc6b34b1fee63f7ec4d2b2a
WeWaWu <wwu5528@gmail.com>
2026-02-05 20:38:14 -0800

    Deleted non-real motors

--- src/main/java/competition/electrical_contract/RoboxContract.java
+++ src/main/java/competition/electrical_contract/RoboxContract.java
@@ -20,41 +20,4 @@ public class RoboxContract extends Contract2026 {
 
     @Override
     public boolean areCanCodersReady() { return false; }
-
-    @Override
-    public boolean isLeftShooterReady() { return false; }
-
-    @Override
-    public boolean isMiddleShooterReady() { return false; }
-
-    @Override
-    public boolean isRightShooterReady() { return false; }
-
-    @Override
-    public CANMotorControllerInfo getLeftShooterMotor() {
-        return new CANMotorControllerInfo("ShooterMotor",
-                MotorControllerType.TalonFx,
-                CANBusId.RIO,
-                918,
-                PDHPort.PDH00,
-                new CANMotorControllerOutputConfig());
-    }
-
-    @Override
-    public CANMotorControllerInfo getMiddleShooterMotor() {
-        return new CANMotorControllerInfo("ShooterMotor",
-                MotorControllerType.TalonFx,
-                CANBusId.RIO,
-                211,
-                new CANMotorControllerOutputConfig());
-    }
-
-    @Override
-    public CANMotorControllerInfo getRightShooterMotor() {
-        return new CANMotorControllerInfo("ShooterMotor",
-                MotorControllerType.TalonFx,
-                CANBusId.RIO,
-                900,
-                new CANMotorControllerOutputConfig());
-    }
 }
