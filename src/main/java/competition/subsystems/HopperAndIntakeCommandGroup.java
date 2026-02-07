774ae062c3a533add9006de16150b84c35d4ece9
WeWaWu <wwu5528@gmail.com>
2026-02-07 14:02:15 -0800

    Fuel intake and Hopper CommandGroup

---
+++ src/main/java/competition/subsystems/HopperAndIntakeCommandGroup.java
@@ -0,0 +1,14 @@
+package competition.subsystems;
+
+import competition.subsystems.fuel_intake.commands.FuelIntakeCommand;
+import competition.subsystems.hopper_roller.HopperRollerSubsystem;
+import xbot.common.command.BaseParallelCommandGroup;
+
+public class HopperAndIntakeCommandGroup extends BaseParallelCommandGroup {
+
+    public HopperAndIntakeCommandGroup(FuelIntakeCommand intake, HopperRollerSubsystem hopper) {
+        addCommands(
+                intake,
+                hopper.getReverseCommand());
+    }
+}
