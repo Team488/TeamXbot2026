7ffd3d96878ea43a7a69f75135dcd1b80a6ee137
WeWaWu <wwu5528@gmail.com>
2026-02-07 15:05:33 -0800

    Changed getReversalCommand -> getEjectCommand

--- src/main/java/competition/subsystems/HopperAndIntakeCommandGroup.java
+++ src/main/java/competition/subsystems/HopperAndIntakeCommandGroup.java
@@ -9,6 +9,6 @@ public class HopperAndIntakeCommandGroup extends BaseParallelCommandGroup {
     public HopperAndIntakeCommandGroup(FuelIntakeCommand intake, HopperRollerSubsystem hopper) {
         addCommands(
                 intake,
-                hopper.getReverseCommand());
+                hopper.getEjectCommand());
     }
 }
