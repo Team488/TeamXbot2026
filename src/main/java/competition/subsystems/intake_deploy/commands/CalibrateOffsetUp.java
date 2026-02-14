03d46df8a1e71c2efa52f3bea8dce14e804e1d5f
JonahIsFake <jsisrael10@gmail.com>
2026-02-14 10:31:24 -0800

    Intake Deploy Calibration (#95)
    * Intake Deploy Calibration
    
    * Comment fixes
    
    ---------
    
    Co-authored-by: Stephen Just <stephenjust@gmail.com>

---
+++ src/main/java/competition/subsystems/intake_deploy/commands/CalibrateOffsetUp.java
@@ -0,0 +1,24 @@
+package competition.subsystems.intake_deploy.commands;
+
+import competition.subsystems.intake_deploy.IntakeDeploySubsystem;
+import xbot.common.command.BaseCommand;
+import javax.inject.Inject;
+
+/**
+ * This is supposed to calibrate the intake when it has not deployed (when it is up)
+ */
+
+public class CalibrateOffsetUp extends BaseCommand{
+
+    final IntakeDeploySubsystem intakeDeploySubsystem;
+
+    @Inject
+    public CalibrateOffsetUp(IntakeDeploySubsystem intakeDeploySubsystem) {
+        this.intakeDeploySubsystem = intakeDeploySubsystem;
+    }
+
+    @Override
+    public void initialize() {
+        intakeDeploySubsystem.calibrateOffsetUp();
+    }
+}
