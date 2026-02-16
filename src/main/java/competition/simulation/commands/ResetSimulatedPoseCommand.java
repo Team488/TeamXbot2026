5f817726bccc2b508091c4bfe515ae47863ef858
David Nguyen <infernaldestinymc@gmail.com>
2026-02-16 11:48:57 -0800

    In progress of making fixies

--- src/main/java/competition/simulation/commands/ResetSimulatedPoseCommand.java
+++ src/main/java/competition/simulation/commands/ResetSimulatedPoseCommand.java
@@ -3,22 +3,24 @@ package competition.simulation.commands;
 import javax.inject.Inject;
 
 import competition.simulation.BaseSimulator;
+import competition.subsystems.pose.PoseSubsystem;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import xbot.common.command.BaseCommand;
 
 public class ResetSimulatedPoseCommand extends BaseCommand {
     BaseSimulator simulator;
+    Pose2d startPose;
 
     @Inject
-    public ResetSimulatedPoseCommand(BaseSimulator simulator) {
+    public ResetSimulatedPoseCommand(BaseSimulator simulator, Pose2d startPose) {
         this.simulator = simulator;
+        this.startPose = startPose;
     }
 
     @Override
     public void initialize() {
-        this.simulator.resetPosition(new Pose2d(3.57,.650, Rotation2d.fromRotations(Math.toRadians(0))));
-        aKitLog.record("Rotation:", Rotation2d.fromDegrees(180));
+        this.simulator.resetPosition(startPose);
     }
 
     @Override
