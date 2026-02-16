48cf87a4e3d77b9d4114d205f64ca2ca4c091cb6
David Nguyen <infernaldestinymc@gmail.com>
2026-02-16 12:11:10 -0800

    Conversion completed

--- src/main/java/competition/command_groups/FireWhenShooterReadyCommandGroup.java
+++ src/main/java/competition/command_groups/FireWhenShooterReadyCommandGroup.java
@@ -1,11 +1,12 @@
-package competition.subsystems.shooter.commands;
+package competition.command_groups;
 
 import competition.subsystems.shooter.ShooterSubsystem;
+import competition.subsystems.shooter.commands.ShooterOutputCommand;
 import competition.subsystems.shooter_feeder.commands.ShooterFeederFire;
 import xbot.common.command.BaseSequentialCommandGroup;
 
-public class FireWhenShooterReady extends BaseSequentialCommandGroup {
-    public FireWhenShooterReady(ShooterOutputCommand shootCommand, ShooterSubsystem shooterSubsystem, ShooterFeederFire shooterFeederFireCommand) {
+public class FireWhenShooterReadyCommandGroup extends BaseSequentialCommandGroup {
+    public FireWhenShooterReadyCommandGroup(ShooterOutputCommand shootCommand, ShooterSubsystem shooterSubsystem, ShooterFeederFire shooterFeederFireCommand) {
         this.addCommands(
                 shootCommand,
                 shooterSubsystem.getWaitForAtGoalCommand(),
