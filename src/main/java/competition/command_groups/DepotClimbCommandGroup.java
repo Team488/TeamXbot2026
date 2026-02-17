e4d54e10a12f8ee776134bf38b214a20c2fc1ed6
David Nguyen <infernaldestinymc@gmail.com>
2026-02-17 12:52:24 -0800

    switch intakeDeploy retractment

--- src/main/java/competition/command_groups/DepotClimbCommandGroup.java
+++ src/main/java/competition/command_groups/DepotClimbCommandGroup.java
@@ -17,8 +17,8 @@ public class DepotClimbCommandGroup extends BaseSequentialCommandGroup {
                                   IntakeDeployRetractCommand intakeDeployRetractCommand) {
         addCommands(
                 readyDepotSideClimbAutoCommand,
-                climberExtendCommand,
                 intakeDeployRetractCommand,
+                climberExtendCommand,
                 depotSideClimbAutoCommand,
                 climberRetractCommand
         );
--- src/main/java/competition/command_groups/OutpostClimbCommandGroup.java
+++ src/main/java/competition/command_groups/OutpostClimbCommandGroup.java
@@ -15,8 +15,8 @@ public class OutpostClimbCommandGroup extends BaseSequentialCommandGroup {
                                     IntakeDeployRetractCommand intakeDeployRetractCommand) {
         addCommands(
                 readyOutpostSideClimbAutoCommand,
-                climberExtendCommand,
                 intakeDeployRetractCommand,
+                climberExtendCommand,
                 outpostSideClimbAutoCommand,
                 climberRetractCommand
         );
