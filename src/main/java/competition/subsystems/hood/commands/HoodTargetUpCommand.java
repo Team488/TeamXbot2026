ca8b0ebb9bbab4d8781f3fbda06c8aad5275c008
David Nguyen <infernaldestinymc@gmail.com>
2026-02-19 20:00:31 -0800

    Implemented

---
+++ src/main/java/competition/subsystems/hood/commands/HoodTargetUpCommand.java
@@ -0,0 +1,27 @@
+package competition.subsystems.hood.commands;
+
+import competition.subsystems.hood.HoodSubsystem;
+import xbot.common.command.BaseCommand;
+
+import javax.inject.Inject;
+
+public class HoodTargetUpCommand extends BaseCommand {
+    final HoodSubsystem hood;
+
+    @Inject
+    public HoodTargetUpCommand(HoodSubsystem hoodSubsystem) {
+        this.hood = hoodSubsystem;
+        addRequirements(hoodSubsystem);
+    }
+
+    @Override
+    public void initialize() {
+        hood.hoodTargetUp();
+    }
+
+    @Override
+    public boolean isFinished() {
+        return true;
+    }
+
+}
