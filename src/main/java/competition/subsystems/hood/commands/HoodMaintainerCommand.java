fa7ff2bd50c848d2d350bc22fb89937398589757
AlanYuan99 <alanyuan709@gmail.com>
2026-02-21 12:00:06 -0800

    Hood Maintainer (#141)
    * most of setpoint subsystem is done
    
    * updated hood subsystem to support servos
    
    * made second servo for hood subsystem
    
    * fixed operator cmd map to support servos
    
    * added checks for servo readiness and being able to run both servos at the same time
    
    * properly set up servo conditions
    
    * fixed robot build issues
    
    * renamed percent to ratio, reduced goal calculation, updated servoZero()
    
    * updated shooter and hood trimming
    
    * set up maintainer and set point system
    
    * updated hood subsystem and created the maintainer
    
    * updated get current value
    
    * added maintainer to default cmd map
    
    * fixed logic

---
+++ src/main/java/competition/subsystems/hood/commands/HoodMaintainerCommand.java
@@ -0,0 +1,55 @@
+package competition.subsystems.hood.commands;
+
+import competition.subsystems.hood.HoodSubsystem;
+import xbot.common.command.BaseMaintainerCommand;
+import xbot.common.command.BaseSetpointSubsystem;
+import xbot.common.logic.HumanVsMachineDecider;
+import xbot.common.properties.PropertyFactory;
+
+import javax.inject.Inject;
+
+public class HoodMaintainerCommand extends BaseMaintainerCommand<Double, Double> {
+
+    final HoodSubsystem hood;
+
+    @Inject
+    public HoodMaintainerCommand(HoodSubsystem hood, PropertyFactory pf, HumanVsMachineDecider.HumanVsMachineDeciderFactory humanVsMachineDeciderFactory) {
+        super(hood, pf, humanVsMachineDeciderFactory, .1, .1);
+        this.hood = hood;
+    }
+
+    @Override
+    public void initialize() {
+        super.initialize();
+    }
+
+    @Override
+    protected void coastAction() {
+
+    }
+
+    @Override
+    protected void calibratedMachineControlAction() {
+        var target = hood.getTargetValue();
+        if (target != null) {
+            hood.runServo();
+        }
+    }
+
+    @Override
+    protected double getErrorMagnitude() {
+        var target = hood.getTargetValue();
+        var current = hood.getCurrentValue();
+        return Math.abs(target - current);
+    }
+
+    @Override
+    protected Double getHumanInput() {
+        return 0.0;
+    }
+
+    @Override
+    protected double getHumanInputMagnitude() {
+        return Math.abs(getHumanInput());
+    }
+}
