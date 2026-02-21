40c1f438ce3e121c620145ead41787f83f43e53c
David Nguyen <infernaldestinymc@gmail.com>
2026-02-21 15:27:11 -0800

    Combining both eject command groups into 1 (#149)
    * Combined
    
    * Reconfigure commands
    
    ---------
    
    Co-authored-by: Stephen Just <stephenjust@gmail.com>

---
+++ src/main/java/competition/command_groups/EjectAllCommandGroup.java
@@ -0,0 +1,23 @@
+package competition.command_groups;
+
+import competition.subsystems.fuel_intake.commands.FuelEjectCommand;
+import competition.subsystems.hopper_roller.HopperRollerSubsystem;
+import competition.subsystems.shooter_feeder.commands.ShooterFeederEject;
+import xbot.common.command.BaseParallelCommandGroup;
+
+import javax.inject.Inject;
+
+public class EjectAllCommandGroup extends BaseParallelCommandGroup {
+
+    @Inject
+    public EjectAllCommandGroup(HopperRollerSubsystem hopper,
+                                ShooterFeederEject shooterFeederEject,
+                                FuelEjectCommand fuelEjectCommand) {
+        addCommands(
+                hopper.getEjectCommand(),
+                shooterFeederEject,
+                fuelEjectCommand
+        );
+    }
+
+}
