92674278f94a29d7b05bcb06c1b9b378b0e99e44
Jun Jie Huang <138041510+NormalDuck@users.noreply.github.com>
2026-03-03 20:09:50 -0800

    feat: commit vibe code

--- src/main/java/competition/subsystems/pose/obstacles/HubObstacle.java
+++ src/main/java/competition/subsystems/pose/obstacles/HubObstacle.java
@@ -1,15 +1,18 @@
 package competition.subsystems.pose.obstacles;
 
-import xbot.common.subsystems.pose.SquareFieldObstacle;
+import xbot.common.subsystems.pose.RectangleFieldObstacle;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.units.Units;
+import edu.wpi.first.units.measure.Distance;
 
 // This is the hub that is not move through.  Using dimensions based on pdf:
 // https://firstfrc.blob.core.windows.net/frc2026/FieldAssets/2026-field-dimension-dwgs.pdf The
 // values are the same between andy mark and welded, but the center is different.  May add 1" to the
 // halfWidth and halfHeight due to there being a +- 2" tolerence here.
-public class HubObstacle extends SquareFieldObstacle {
+public class HubObstacle extends RectangleFieldObstacle {
+    public static final Distance HALF_SIZE = Units.Inches.of(23.5);
+
     public HubObstacle(Translation2d center) {
-        super(center, Units.Inches.of(23.5), true);
+        super(center, HALF_SIZE, HALF_SIZE, true);
     }
 }
