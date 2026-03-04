6447bacb8507b0b53cc0b9d4949ea9073e0520f4
WeWaWu <wwu5528@gmail.com>
2026-03-03 19:30:07 -0800

    Just main

--- src/main/java/competition/command_groups/PrepareToShootCommandGroup.java
+++ src/main/java/competition/command_groups/PrepareToShootCommandGroup.java
@@ -1,11 +1,31 @@
 package competition.command_groups;
 
+import competition.subsystems.hood.commands.HoodSetCommand;
+import competition.subsystems.shooter.commands.ShooterOutputCommand;
+import edu.wpi.first.units.measure.AngularVelocity;
 import xbot.common.command.BaseParallelCommandGroup;
 
 import javax.inject.Inject;
 
 public class PrepareToShootCommandGroup extends BaseParallelCommandGroup {
 
+    HoodSetCommand hoodSetCommand;
+    ShooterOutputCommand outputCommand;
+
+    public void setShooterGoal(AngularVelocity targetVelocity) {
+        this.outputCommand.setTargetVelocity(targetVelocity);
+    }
+
+    public void setHoodGoal(double ratio) {
+        this.hoodSetCommand.setTargetRatio(ratio);
+    }
+
     @Inject
+    public PrepareToShootCommandGroup(HoodSetCommand hoodSet, ShooterOutputCommand shooterOutput) {
 
+        this.outputCommand = shooterOutput;
+        this.hoodSetCommand = hoodSet;
 
+        this.addCommands(shooterOutput, hoodSet);
+    }
+}
