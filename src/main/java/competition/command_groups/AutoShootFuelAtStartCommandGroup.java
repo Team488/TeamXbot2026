740b655d2a8cdc68024ba1e4b96d865cebc75e99
WeWaWu <wwu5528@gmail.com>
2026-03-05 19:04:35 -0800

    Fixes

--- src/main/java/competition/command_groups/AutoShootFuelAtStartCommandGroup.java
+++ src/main/java/competition/command_groups/AutoShootFuelAtStartCommandGroup.java
@@ -1,13 +1,22 @@
 package competition.command_groups;
 
 import xbot.common.command.BaseSequentialCommandGroup;
+import xbot.common.properties.DoubleProperty;
+import xbot.common.properties.PropertyFactory;
 
 import javax.inject.Inject;
 
 public class AutoShootFuelAtStartCommandGroup extends BaseSequentialCommandGroup {
 
+    final DoubleProperty fireTimeoutSec;
+
     @Inject
-    public AutoShootFuelAtStartCommandGroup(PrepareToShootCommandGroup prepare, FireWhenShooterReadyCommandGroup fire) {
-        addCommands(prepare, fire.withTimeout(2.0));
+    public AutoShootFuelAtStartCommandGroup(PrepareToShootCommandGroup prepare, FireWhenShooterReadyCommandGroup fire,
+                                            PropertyFactory pf) {
+        fireTimeoutSec = pf.createPersistentProperty("Fire Timeout", 1.0);
+
+        addCommands(prepare,
+                    fire.withTimeout(fireTimeoutSec.get())
+        );
     }
 }
--- src/main/java/competition/command_groups/PrepareToShootCommandGroup.java
+++ src/main/java/competition/command_groups/PrepareToShootCommandGroup.java
@@ -1,6 +1,8 @@
 package competition.command_groups;
 
+import competition.subsystems.hood.HoodSubsystem;
 import competition.subsystems.hood.commands.HoodSetCommand;
+import competition.subsystems.shooter.ShooterSubsystem;
 import competition.subsystems.shooter.commands.ShooterOutputCommand;
 import edu.wpi.first.units.measure.AngularVelocity;
 import xbot.common.command.BaseParallelCommandGroup;
@@ -21,11 +23,15 @@ public class PrepareToShootCommandGroup extends BaseParallelCommandGroup {
     }
 
     @Inject
-    public PrepareToShootCommandGroup(HoodSetCommand hoodSet, ShooterOutputCommand shooterOutput) {
+    public PrepareToShootCommandGroup(HoodSubsystem hood, HoodSetCommand hoodSet, ShooterSubsystem shooter,
+                                      ShooterOutputCommand shooterOutput) {
+
+        var hoodWaitCommand = hood.getWaitForAtGoalCommand();
+        var shooterWaitCommand = shooter.getWaitForAtGoalCommand();
 
         this.outputCommand = shooterOutput;
         this.hoodSetCommand = hoodSet;
 
-        this.addCommands(shooterOutput, hoodSet);
+        this.addCommands(shooterOutput, shooterWaitCommand, hoodSet, hoodWaitCommand);
     }
 }
