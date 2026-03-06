99256289c0bedced150fdfaad32365ba38c4379e
Victor C <vich2021@gmail.com>
2026-03-06 09:51:12 -0800

    Update FireWhenReadyAndRetractIntakeDeployCommandGroup.java

--- src/main/java/competition/command_groups/FireWhenReadyAndRetractIntakeDeployCommandGroup.java
+++ src/main/java/competition/command_groups/FireWhenReadyAndRetractIntakeDeployCommandGroup.java
@@ -2,6 +2,7 @@ package competition.command_groups;
 
 import competition.subsystems.intake_deploy.commands.IntakeDeployRetractCommand;
 import xbot.common.command.BaseParallelCommandGroup;
+import competition.command_groups.FireWhenReadyShooterCommandGroup;
 
 import javax.inject.Inject;
 
@@ -10,8 +11,8 @@ import javax.inject.Inject;
 public class FireWhenReadyAndRetractIntakeDeployCommandGroup extends BaseParallelCommandGroup {
 
     @Inject
-    public FireWhenReadyAndRetractIntakeDeployCommandGroup(FireWhenShooterReadyCommandGroup fireWhenShooterReadyCommandGroup,
+    public FireWhenReadyAndRetractIntakeDeployCommandGroup(FireWhenReadyShooterCommandGroup fireWhenReadyShooterCommandGroup,
                                                            IntakeDeployRetractCommand intakeDeployRetractCommand) {
-        this.addCommands(fireWhenShooterReadyCommandGroup, intakeDeployRetractCommand);
+        this.addCommands(fireWhenReadyShooterCommandGroup, intakeDeployRetractCommand);
     }
 }
