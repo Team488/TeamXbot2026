3a3aee10469a880d910c6ad9157b40676ac003d0
Lawrence Wu <yiwu0819@gmail.com>
2026-03-07 13:07:50 -0800

    revert: not set hood target in drive command

--- src/main/java/competition/command_groups/DriveToShootingPositionCommand.java
+++ src/main/java/competition/command_groups/DriveToShootingPositionCommand.java
@@ -1,10 +1,8 @@
 package competition.command_groups;
 
 import competition.subsystems.drive.DriveSubsystem;
-import competition.subsystems.hood.HoodSubsystem;
 import competition.subsystems.pose.AutoLandmarks;
 import competition.subsystems.pose.PoseSubsystem;
-import competition.subsystems.pose.TrajectoriesCalculation;
 import xbot.common.logging.RobotAssertionManager;
 import xbot.common.properties.PropertyFactory;
 import xbot.common.subsystems.drive.SwervePointKinematics;
@@ -20,21 +18,16 @@ import java.util.List;
 
 public class DriveToShootingPositionCommand extends SwerveSimpleBezierCommand {
     private final DriveSubsystem drive;
-    private final HoodSubsystem hood;
     private final PoseSubsystem pose;
-    private final TrajectoriesCalculation trajectoriesCalculation;
     private final AutoLandmarks autoLandmarks;
 
     @Inject
     public DriveToShootingPositionCommand(DriveSubsystem drive, PoseSubsystem pose,
                                           PropertyFactory pf, HeadingModule.HeadingModuleFactory headingModuleFactory,
-                                          RobotAssertionManager robotAssertionManager, HoodSubsystem hood,
-                                          TrajectoriesCalculation trajectoriesCalculation, AutoLandmarks autoLandmarks) {
+                                          RobotAssertionManager robotAssertionManager, AutoLandmarks autoLandmarks) {
         super(drive, pose, pf, headingModuleFactory, robotAssertionManager);
         this.drive = drive;
         this.pose = pose;
-        this.hood = hood;
-        this.trajectoriesCalculation = trajectoriesCalculation;
         this.autoLandmarks = autoLandmarks;
     }
 
@@ -43,10 +36,6 @@ public class DriveToShootingPositionCommand extends SwerveSimpleBezierCommand {
         var currentPose = this.pose.getCurrentPose2d();
         var startPose = this.autoLandmarks.getAllianceShootingStartingPose(currentPose);
         var endPose = this.autoLandmarks.getClosestShootingPose(startPose);
-
-        hood.setTargetValue(trajectoriesCalculation.calculateAllianceHubShootingData(endPose)
-                .servoRatio()
-        );
 
         List<XbotSwervePoint> swervePoints = new ArrayList<>();
         swervePoints.add(new XbotSwervePoint(endPose, 0.001));
