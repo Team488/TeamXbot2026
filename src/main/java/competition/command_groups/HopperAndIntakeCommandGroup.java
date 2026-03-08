88149822d735f706d515c2432636f53b73fff4cf
Stephen Just <stephenjust@gmail.com>
2026-03-07 17:08:02 -0800

    Force intake down while collecting

--- src/main/java/competition/command_groups/HopperAndIntakeCommandGroup.java
+++ src/main/java/competition/command_groups/HopperAndIntakeCommandGroup.java
@@ -2,6 +2,8 @@ package competition.command_groups;
 
 import competition.subsystems.collector_intake.commands.CollectorIntakeCommand;
 import competition.subsystems.hopper_roller.HopperRollerSubsystem;
+import competition.subsystems.intake_deploy.IntakeDeploySubsystem;
+import competition.subsystems.intake_deploy.commands.IntakeDeployExtendWithoutPidCommand;
 import xbot.common.command.BaseParallelCommandGroup;
 
 import javax.inject.Inject;
@@ -9,7 +11,11 @@ import javax.inject.Inject;
 public class HopperAndIntakeCommandGroup extends BaseParallelCommandGroup {
 
     @Inject
-    public HopperAndIntakeCommandGroup(HopperRollerSubsystem hopperRoller, CollectorIntakeCommand fuelIntakeCommand) {
-        addCommands(fuelIntakeCommand, hopperRoller.getIntakeCommand());
+    public HopperAndIntakeCommandGroup(HopperRollerSubsystem hopperRoller,
+                                       CollectorIntakeCommand fuelIntakeCommand,
+                                       IntakeDeploySubsystem intakeDeploy,
+                                       IntakeDeployExtendWithoutPidCommand intakeDown) {
+        intakeDown.setPower(intakeDeploy.collectionDownwardPressure.get());
+        addCommands(intakeDown, fuelIntakeCommand, hopperRoller.getIntakeCommand());
     }
 }
