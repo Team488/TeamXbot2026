dd7c0296f08c51ac579475e0bef5a4ea4239afef
Saqib Rokadia <saqib@saqibr.com>
2026-03-09 17:10:48 -0700

    Moving preset to trajectories calculations for consistency (#247)
    * Moving preset trajectories
    
    * Switching over to using TrajectoriesCalc for using presets.
    
    * Updates from Josh.
    
    ---------
    
    Co-authored-by: Stephen Just <stephenjust@gmail.com>

--- src/main/java/competition/command_groups/PrepareToShootCommandGroup.java
+++ src/main/java/competition/command_groups/PrepareToShootCommandGroup.java
@@ -1,6 +1,7 @@
 package competition.command_groups;
 
 import competition.subsystems.hood.commands.HoodSetCommand;
+import competition.subsystems.pose.TrajectoriesCalculation;
 import competition.subsystems.shooter.commands.ShooterOutputCommand;
 import edu.wpi.first.units.measure.AngularVelocity;
 import xbot.common.command.BaseParallelCommandGroup;
@@ -9,6 +10,7 @@ import javax.inject.Inject;
 
 public class PrepareToShootCommandGroup extends BaseParallelCommandGroup {
 
+    private final TrajectoriesCalculation trajectoriesCalculation;
     HoodSetCommand hoodSetCommand;
     ShooterOutputCommand outputCommand;
 
@@ -22,9 +24,16 @@ public class PrepareToShootCommandGroup extends BaseParallelCommandGroup {
         return this;
     }
 
-    @Inject
-    public PrepareToShootCommandGroup(HoodSetCommand hoodSet, ShooterOutputCommand shooterOutput) {
+    public PrepareToShootCommandGroup setPresetLocation(TrajectoriesCalculation.PresetShootingDistance presetShootingDistance) {
+        var settings = this.trajectoriesCalculation.getPresetShootingSettings(presetShootingDistance);
+        this.outputCommand.setTargetVelocity(settings.shooterRPM());
+        this.hoodSetCommand.setTargetRatio(settings.hoodServoRatio());
+        return this;
+    }
 
+    @Inject
+    public PrepareToShootCommandGroup(HoodSetCommand hoodSet, ShooterOutputCommand shooterOutput, TrajectoriesCalculation trajectoriesCalculation) {
+        this.trajectoriesCalculation = trajectoriesCalculation;
         this.outputCommand = shooterOutput;
         this.hoodSetCommand = hoodSet;
 
