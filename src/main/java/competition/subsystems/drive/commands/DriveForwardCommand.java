e9f93849f2f48b447effd45fcbe9f7355c5e246f
Clulogh <197954988+Clulogh@users.noreply.github.com>
2026-03-12 20:16:25 -0700

    Update previous code - leftout

--- src/main/java/competition/subsystems/drive/commands/DriveForwardCommand.java
+++ src/main/java/competition/subsystems/drive/commands/DriveForwardCommand.java
@@ -44,6 +44,7 @@ public class DriveForwardCommand extends SwerveSimpleTrajectoryCommand {
     }
 
     public double currentHeading() {
+        return pose.getCurrentHeading().getDegrees();
     }
 
     public void setTarget(double d) {
