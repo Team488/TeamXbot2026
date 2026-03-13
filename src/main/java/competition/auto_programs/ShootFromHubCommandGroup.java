63ae782ea0a73a60ae69c19fc04f24323d63a14d
Saqib Rokadia <saqib@saqibr.com>
2026-03-12 17:56:28 -0700

    Making hub shooting auto (#266)
    * Adding ShootFromHub Command
    
    * Adding ShootFromHub Command to elastic
    
    * Mirroring Stephen's change from ShootFromTrench.
    
    * Reverting auto formatting.

---
+++ src/main/java/competition/auto_programs/ShootFromHubCommandGroup.java
@@ -0,0 +1,30 @@
+package competition.auto_programs;
+
+import competition.auto_programs.BaseAutonomousSequentialCommandGroup;
+import competition.command_groups.FireWhenReadyShooterCommandGroup;
+import competition.command_groups.PrepareToShootCommandGroup;
+import competition.subsystems.intake_deploy.commands.IntakeDeployAutoCalibrateCommandFactory;
+import competition.subsystems.pose.TrajectoriesCalculation;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import xbot.common.subsystems.autonomous.AutonomousCommandSelector;
+
+import javax.inject.Inject;
+
+public class ShootFromHubCommandGroup extends BaseAutonomousSequentialCommandGroup {
+
+    @Inject
+    public ShootFromHubCommandGroup(AutonomousCommandSelector autoSelector,
+            TrajectoriesCalculation trajectoriesCalculation,
+            FireWhenReadyShooterCommandGroup fireWhenReadyShooterCommandGroup,
+            PrepareToShootCommandGroup prepareToShootCommandGroup,
+            IntakeDeployAutoCalibrateCommandFactory intakeDeployAutoCalibrateCommandFactory) {
+        super(autoSelector);
+
+        getAutoStatusChangeCommand("Starting ShootFromHubCommandGroup");
+        var intakeCalibrationCommand = intakeDeployAutoCalibrateCommandFactory.create();
+        prepareToShootCommandGroup.setPresetLocation(TrajectoriesCalculation.PresetShootingDistance.NEAR);
+        var calibrateAndShoot = new ParallelCommandGroup(intakeCalibrationCommand, prepareToShootCommandGroup, fireWhenReadyShooterCommandGroup);
+
+        this.addCommands(calibrateAndShoot);
+    }
+}
