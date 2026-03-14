f0170ca862d3ab29540e9ad917c72faf8e159de1
Stephen Just <stephenjust@gmail.com>
2026-03-14 12:06:58 -0700

    Add timeouts to auto commands so we don't kill the battery in the first 20s of the match. (#276)

--- src/main/java/competition/auto_programs/ShootFromHubCommandGroup.java
+++ src/main/java/competition/auto_programs/ShootFromHubCommandGroup.java
@@ -6,24 +6,33 @@ import competition.command_groups.PrepareToShootCommandGroup;
 import competition.subsystems.intake_deploy.commands.IntakeDeployAutoCalibrateCommandFactory;
 import competition.subsystems.pose.TrajectoriesCalculation;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import xbot.common.properties.DoubleProperty;
+import xbot.common.properties.PropertyFactory;
 import xbot.common.subsystems.autonomous.AutonomousCommandSelector;
 
 import javax.inject.Inject;
 
 public class ShootFromHubCommandGroup extends BaseAutonomousSequentialCommandGroup {
 
+    private final DoubleProperty timeout;
+
     @Inject
     public ShootFromHubCommandGroup(AutonomousCommandSelector autoSelector,
             TrajectoriesCalculation trajectoriesCalculation,
             FireWhenReadyShooterCommandGroup fireWhenReadyShooterCommandGroup,
             PrepareToShootCommandGroup prepareToShootCommandGroup,
-            IntakeDeployAutoCalibrateCommandFactory intakeDeployAutoCalibrateCommandFactory) {
+            IntakeDeployAutoCalibrateCommandFactory intakeDeployAutoCalibrateCommandFactory,
+            PropertyFactory pf) {
         super(autoSelector);
+
+        pf.setPrefix(this.getName());
+        this.timeout = pf.createPersistentProperty("TimeoutSeconds", 5.0);
 
         getAutoStatusChangeCommand("Starting ShootFromHubCommandGroup");
         var intakeCalibrationCommand = intakeDeployAutoCalibrateCommandFactory.create();
         prepareToShootCommandGroup.setPresetLocation(TrajectoriesCalculation.PresetShootingDistance.NEAR);
-        var calibrateAndShoot = new ParallelCommandGroup(intakeCalibrationCommand, prepareToShootCommandGroup, fireWhenReadyShooterCommandGroup);
+        var calibrateAndShoot = new ParallelCommandGroup(intakeCalibrationCommand, prepareToShootCommandGroup, fireWhenReadyShooterCommandGroup)
+                .withTimeout(timeout.get());
 
         this.addCommands(calibrateAndShoot);
     }
