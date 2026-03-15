0e6633f053a1fa531029cac5a7537d8ad655877e
Saqib Rokadia <saqib@saqibr.com>
2026-03-15 13:01:43 -0700

    Adding a shoot from trench then move through neutral zone. (#285)
    * Adding a shoot from trench then move through neutral zone.
    
    * Adding button in robot layout.
    
    * Adding a finished when shooting spin down command.

---
+++ src/main/java/competition/command_groups/NoWaitFinishedShootingCommand.java
@@ -0,0 +1,30 @@
+package competition.command_groups;
+
+import javax.inject.Inject;
+
+import static edu.wpi.first.units.Units.RPM;
+import competition.subsystems.collector_intake.commands.CollectorIntakeCommand;
+import competition.subsystems.hood.HoodSubsystem;
+import competition.subsystems.hopper_roller.HopperRollerSubsystem;
+import competition.subsystems.shooter.ShooterSubsystem;
+import competition.subsystems.shooter.commands.ShooterOutputCommand;
+import competition.subsystems.shooter_feeder.commands.ShooterFeederFire;
+import xbot.common.command.BaseParallelCommandGroup;
+import xbot.common.command.NamedInstantCommand;
+
+public class NoWaitFinishedShootingCommand extends BaseParallelCommandGroup {
+
+    @Inject
+    public NoWaitFinishedShootingCommand(HopperRollerSubsystem hopper,
+            ShooterSubsystem shooterSubsystem,
+            HoodSubsystem hoodSubsystem,
+            ShooterOutputCommand shooterOutputCommand,
+            ShooterFeederFire shooterFeederFireCommand,
+            CollectorIntakeCommand fuelIntakeCommand) {
+        var setHoodCommand = new NamedInstantCommand("Set Hood Min", () -> hoodSubsystem.setTargetValue(0.0));
+        var setShooterGoal = new NamedInstantCommand("Set Shooter Stop", () -> shooterSubsystem.setTargetValue(RPM.of(0.0)));
+        var setHopperRoller = new NamedInstantCommand("Set Hopper Roller Stop", () -> hopper.stop());
+
+        this.addCommands(setHoodCommand, setShooterGoal, setHopperRoller);
+    }
+}
