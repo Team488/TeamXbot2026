c9dc9a5333b9b8a5fdf381c998e0039bb2c634d6
Stephen Just <stephenjust@gmail.com>
2026-03-18 22:13:14 -0700

    Update the commands to stop intake mech during auto

--- src/main/java/competition/command_groups/NoWaitFinishedShootingCommand.java
+++ src/main/java/competition/command_groups/NoWaitFinishedShootingCommand.java
@@ -3,12 +3,18 @@ package competition.command_groups;
 import javax.inject.Inject;
 
 import static edu.wpi.first.units.Units.RPM;
+
+import competition.Robot;
 import competition.subsystems.collector_intake.commands.CollectorIntakeCommand;
+import competition.subsystems.collector_intake.commands.CollectorStopCommand;
 import competition.subsystems.hood.HoodSubsystem;
+import competition.subsystems.hood.commands.HoodSetCommand;
 import competition.subsystems.hopper_roller.HopperRollerSubsystem;
 import competition.subsystems.shooter.ShooterSubsystem;
 import competition.subsystems.shooter.commands.ShooterOutputCommand;
+import competition.subsystems.shooter.commands.ShooterStopCommand;
 import competition.subsystems.shooter_feeder.commands.ShooterFeederFire;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import xbot.common.command.BaseParallelCommandGroup;
 import xbot.common.command.NamedInstantCommand;
 
@@ -16,15 +22,14 @@ public class NoWaitFinishedShootingCommand extends BaseParallelCommandGroup {
 
     @Inject
     public NoWaitFinishedShootingCommand(HopperRollerSubsystem hopper,
-            ShooterSubsystem shooterSubsystem,
-            HoodSubsystem hoodSubsystem,
-            ShooterOutputCommand shooterOutputCommand,
-            ShooterFeederFire shooterFeederFireCommand,
-            CollectorIntakeCommand fuelIntakeCommand) {
-        var setHoodCommand = new NamedInstantCommand("Set Hood Min", () -> hoodSubsystem.setTargetValue(0.0));
-        var setShooterGoal = new NamedInstantCommand("Set Shooter Stop", () -> shooterSubsystem.setTargetValue(RPM.of(0.0)));
-        var setHopperRoller = new NamedInstantCommand("Set Hopper Roller Stop", () -> hopper.stop());
+             ShooterStopCommand shooterStopCommand,
+             HoodSetCommand setHoodCommand,
+             CollectorStopCommand collectorStopCommand) {
+        setHoodCommand.setTargetRatio(0.0);
 
-        this.addCommands(setHoodCommand, setShooterGoal, setHopperRoller);
+        var group = new ParallelCommandGroup(setHoodCommand, shooterStopCommand, hopper.getStopCommand(), collectorStopCommand);
+
+        // Stop after one cycle as some of these commands won't end
+        addCommands(group.withTimeout(Robot.LOOP_INTERVAL));
     }
 }
