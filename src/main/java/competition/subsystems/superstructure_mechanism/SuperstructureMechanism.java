fa34060fe1d538db114ff4cd6331be57f7d60bc6
Alex Schokking <aschokking@gmail.com>
2026-03-24 19:52:48 -0700

    initial commit adding mechanism

---
+++ src/main/java/competition/subsystems/superstructure_mechanism/SuperstructureMechanism.java
@@ -0,0 +1,65 @@
+package competition.subsystems.superstructure_mechanism;
+
+import competition.subsystems.hood.HoodSubsystem;
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.wpilibj.util.Color;
+import edu.wpi.first.wpilibj.util.Color8Bit;
+import org.littletonrobotics.junction.mechanism.LoggedMechanism2d;
+import org.littletonrobotics.junction.mechanism.LoggedMechanismLigament2d;
+
+import static edu.wpi.first.units.Units.Degrees;
+
+public class SuperstructureMechanism {
+    final LoggedMechanism2d mech2d;
+    final LoggedMechanismLigament2d hoodLigament;
+    final LoggedMechanismLigament2d intakeLigament;
+
+    double hoodNormalizedPosition = 0;
+    double intakeAngleDegrees = 10;
+
+    // Hood ligament represents the deflector piece at the shooter exit
+    final double hoodLigamentLengthMeters = 0.25;
+
+    // Intake ligament represents the deploy arm
+    final double intakeLigamentLengthMeters = 0.45;
+
+    public SuperstructureMechanism() {
+        this.mech2d = new LoggedMechanism2d(2, 2);
+
+        // Hood: shooter-side of the robot (right side)
+        var hoodRoot = mech2d.getRoot("HoodRoot", 1.3, 0.45);
+        this.hoodLigament = new LoggedMechanismLigament2d(
+                "hood",
+                hoodLigamentLengthMeters,
+                HoodSubsystem.mechanismAngleMin,
+                5,
+                new Color8Bit(Color.kOrange));
+        hoodRoot.append(hoodLigament);
+
+        // Intake deploy: front of the robot (left side)
+        var intakeRoot = mech2d.getRoot("IntakeRoot", 0.3, 0.3);
+        this.intakeLigament = new LoggedMechanismLigament2d(
+                "intake",
+                intakeLigamentLengthMeters,
+                10,
+                5,
+                new Color8Bit(Color.kGreen));
+        intakeRoot.append(intakeLigament);
+    }
+
+    public void setHoodNormalizedPosition(double normalized) {
+        hoodNormalizedPosition = normalized;
+    }
+
+    public void setIntakeAngle(Angle angle) {
+        intakeAngleDegrees = angle.in(Degrees);
+    }
+
+    public LoggedMechanism2d getMechanism() {
+        double hoodAngle = HoodSubsystem.mechanismAngleMin
+                + hoodNormalizedPosition * (HoodSubsystem.mechanismAngleMax - HoodSubsystem.mechanismAngleMin);
+        hoodLigament.setAngle(hoodAngle);
+        intakeLigament.setAngle(intakeAngleDegrees);
+        return mech2d;
+    }
+}
---
+++ src/main/java/competition/subsystems/superstructure_mechanism/SuperstructureMechanismSubsystem.java
@@ -0,0 +1,34 @@
+package competition.subsystems.superstructure_mechanism;
+
+import competition.subsystems.hood.HoodSubsystem;
+import competition.subsystems.intake_deploy.IntakeDeploySubsystem;
+import xbot.common.command.BaseSubsystem;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+/**
+ * Responsible for rendering a Mechanism2d representing the hood and intake deploy as perceived by
+ * the robot code.
+ */
+@Singleton
+public class SuperstructureMechanismSubsystem extends BaseSubsystem {
+    final HoodSubsystem hoodSubsystem;
+    final IntakeDeploySubsystem intakeDeploySubsystem;
+    final SuperstructureMechanism mechanism;
+
+    @Inject
+    public SuperstructureMechanismSubsystem(HoodSubsystem hoodSubsystem,
+                                            IntakeDeploySubsystem intakeDeploySubsystem) {
+        this.hoodSubsystem = hoodSubsystem;
+        this.intakeDeploySubsystem = intakeDeploySubsystem;
+        this.mechanism = new SuperstructureMechanism();
+    }
+
+    @Override
+    public void periodic() {
+        mechanism.setHoodNormalizedPosition(hoodSubsystem.getCurrentValue());
+        mechanism.setIntakeAngle(intakeDeploySubsystem.getCurrentValue());
+        aKitLog.record("SuperstructureMechanism", mechanism.getMechanism());
+    }
+}
