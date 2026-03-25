12d310c636953339b355935b2e077d2481af7574
Jonah I <jsisrael10@gmail.com>
2026-03-24 19:34:48 -0700

    Changed error red so it is not the same as the hood thing

--- src/main/java/competition/subsystems/lights/LightsSubsystem.java
+++ src/main/java/competition/subsystems/lights/LightsSubsystem.java
@@ -52,7 +52,7 @@ public class LightsSubsystem extends BaseSubsystem {
         } else if (intakeDeploy.isCalibrated && DriverStation.isTeleop()) {
             lights.larson(0, Hertz.of(25), Color.kGreen, LarsonBounceValue.Back);
         } else if (DriverStation.isDisabled() || !intakeDeploy.isCalibrated || !voltageMonitor.isAtHealthyVoltage()) {
-            lights.larson(0, Hertz.of(25), Color.kDarkRed, LarsonBounceValue.Back);
+            lights.larson(0, Hertz.of(25), Color.kFirstRed, LarsonBounceValue.Back);
         } else {
             lights.larson(0, Hertz.of(25), Color.kWhite, LarsonBounceValue.Back);
         }
