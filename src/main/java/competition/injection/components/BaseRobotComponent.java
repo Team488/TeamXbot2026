719c294e4bbffee5943677d7159dfef3eac09eac
David Nguyen <infernaldestinymc@gmail.com>
2026-03-26 20:32:23 -0700

    Unit tests added

--- src/main/java/competition/injection/components/BaseRobotComponent.java
+++ src/main/java/competition/injection/components/BaseRobotComponent.java
@@ -11,6 +11,7 @@ import competition.subsystems.hood.HoodSubsystem;
 import competition.subsystems.hopper_roller.HopperRollerSubsystem;
 import competition.subsystems.intake_deploy.IntakeDeploySubsystem;
 import competition.subsystems.lights.LightsSubsystem;
+import competition.subsystems.pose.TrajectoriesCalculation;
 import competition.subsystems.shooter.ShooterSubsystem;
 import competition.subsystems.shooter.commands.WhenShooterReadyRumbleCommand;
 import competition.subsystems.shooter_feeder.ShooterFeederSubsystem;
@@ -59,4 +60,7 @@ public abstract class BaseRobotComponent extends BaseComponent {
     public abstract OperatorInterface operatorInterface();
 
     public abstract WhenShooterReadyRumbleCommand whenShooterReadyRumbleCommand();
+
+    public abstract TrajectoriesCalculation trajectoriesCalculation();
+
 }
---
+++ src/test/java/competition/subsystems/shooter/TrajectoriesCalculationTest.java
@@ -0,0 +1,83 @@
+package competition.subsystems.shooter;
+
+import competition.BaseCompetitionTest;
+import competition.subsystems.pose.Landmarks;
+import competition.subsystems.pose.TrajectoriesCalculation;
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTagFields;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.units.Units;
+import edu.wpi.first.wpilibj.DriverStation;
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+public class TrajectoriesCalculationTest extends BaseCompetitionTest {
+
+    public Pose2d hub = Landmarks.getAllianceHubPose(
+            AprilTagFieldLayout.loadField(AprilTagFields.k2026RebuiltWelded), DriverStation.Alliance.Blue);
+
+    @Test
+    public void testTrajectoriesCalculation() {
+        TrajectoriesCalculation calc = getInjectorComponent().trajectoriesCalculation();
+
+        // Test that we get valid shooting data back at a known distance
+        Pose2d robotPose = new Pose2d(2.0, 0.0, new Rotation2d(0));
+        TrajectoriesCalculation.ShootingData data = calc.calculateAllianceHubShootingData(robotPose);
+
+        // Make sure we got something back and not empty/zero data
+        assertNotNull(data);
+        assertTrue("RPM should be greater than 0", data.shooterRPM().in(Units.RPM) > 0);
+    }
+
+    @Test
+    public void testInterpolationBetweenTwoPoints() {
+        TrajectoriesCalculation calc = getInjectorComponent().trajectoriesCalculation();
+
+        // Use distances in the JSON range (3.797 to 6.105)
+        Pose2d closePose = new Pose2d(hub.getX() + 3.8, hub.getY(), new Rotation2d(0));
+        Pose2d farPose = new Pose2d(hub.getX() + 6.0, hub.getY(), new Rotation2d(0));
+        Pose2d midPose = new Pose2d(hub.getX() + 4.9, hub.getY(), new Rotation2d(0));
+
+        TrajectoriesCalculation.ShootingData closeData = calc.calculateAllianceHubShootingData(closePose);
+        TrajectoriesCalculation.ShootingData farData = calc.calculateAllianceHubShootingData(farPose);
+        TrajectoriesCalculation.ShootingData midData = calc.calculateAllianceHubShootingData(midPose);
+
+        double closeServo = closeData.servoRatio();
+        double farServo = farData.servoRatio();
+        double midServo = midData.servoRatio();
+
+        // Servo increases with distance so mid should be between close and far
+        assertTrue("Interpolated servo should be between close and far",
+                midServo > closeServo && midServo < farServo);
+    }
+
+    @Test
+    public void testTenRandomPointsInAllianceZone() {
+        TrajectoriesCalculation calc = getInjectorComponent().trajectoriesCalculation();
+
+        for (int i = 0; i < 10; i++){
+
+            double randomWidth = (Math.random() * 4.03); //
+            double randomHeight = (Math.random() * 8.07);
+            Pose2d randomPose = new Pose2d(randomWidth, randomHeight, new Rotation2d(0));
+
+            TrajectoriesCalculation.ShootingData data = calc.calculateAllianceHubShootingData(randomPose);
+
+            assertNotNull(data);
+
+            if (data.servoRatio() > 0) {
+                assertTrue("Servo should be between 0.2 and 1.0 to see if it goes ni",
+                        data.servoRatio() >= 0.2 && data.servoRatio() <= 1.0);
+            }
+
+        }
+
+    }
+
+
+}
+
+
