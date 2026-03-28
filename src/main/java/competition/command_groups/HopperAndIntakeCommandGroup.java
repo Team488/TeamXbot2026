0140242fcf2cc6076f275de006b6f1adf46cd52d
Alex Schokking <aschokking@gmail.com>
2026-03-28 13:29:28 -0700

    Revert group change

--- src/main/java/competition/command_groups/HopperAndIntakeCommandGroup.java
+++ src/main/java/competition/command_groups/HopperAndIntakeCommandGroup.java
@@ -2,7 +2,6 @@ package competition.command_groups;
 
 import competition.subsystems.collector_intake.commands.CollectorIntakeCommand;
 import competition.subsystems.hopper_roller.HopperRollerSubsystem;
-import competition.subsystems.intake_deploy.commands.IntakeDeployOscillateForCollectionCommand;
 import xbot.common.command.BaseParallelCommandGroup;
 
 import javax.inject.Inject;
@@ -11,8 +10,7 @@ public class HopperAndIntakeCommandGroup extends BaseParallelCommandGroup {
 
     @Inject
     public HopperAndIntakeCommandGroup(HopperRollerSubsystem hopperRoller,
-                                       CollectorIntakeCommand fuelIntakeCommand,
-                                       IntakeDeployOscillateForCollectionCommand intakeOscillationCommand) {
-        addCommands(fuelIntakeCommand, hopperRoller.getIntakeCommand(), intakeOscillationCommand);
+                                       CollectorIntakeCommand fuelIntakeCommand) {
+        addCommands(fuelIntakeCommand, hopperRoller.getCollectCommand());
     }
 }
