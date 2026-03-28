1a90794b98336c04479744d21a999cb6bc13b7b8
Alex Schokking <aschokking@gmail.com>
2026-03-28 12:34:29 -0700

    Try a test failure

--- src/test/java/competition/RobotInitTest.java
+++ src/test/java/competition/RobotInitTest.java
@@ -7,5 +7,6 @@ public class RobotInitTest extends BaseCompetitionTest {
     public void testDefaultSystem() {
         getInjectorComponent().subsystemDefaultCommandMap();
         getInjectorComponent().operatorCommandMap();
+        throw new RuntimeException("This test just checks that the robot can initialize without throwing exceptions");
     }
 }
