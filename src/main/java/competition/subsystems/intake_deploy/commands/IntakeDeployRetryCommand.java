78b8eded43a5ca7028878dfc973c949b0a626324
Clulogh <197954988+Clulogh@users.noreply.github.com>
2026-03-28 14:30:17 -0700

    Added intake deploy retry logic
    also added timeout

--- src/main/java/competition/subsystems/intake_deploy/commands/IntakeDeployRetryCommand.java
+++ src/main/java/competition/subsystems/intake_deploy/commands/IntakeDeployRetryCommand.java
@@ -1,18 +1,32 @@
 package competition.subsystems.intake_deploy.commands;
 
 import competition.subsystems.intake_deploy.IntakeDeploySubsystem;
+import edu.wpi.first.wpilibj.Timer;
+import org.json.Property;
 import xbot.common.command.BaseCommand;
+import xbot.common.command.SimpleWaitForMaintainerCommand;
+import xbot.common.properties.DoubleProperty;
+import xbot.common.properties.PropertyFactory;
 
 public class IntakeDeployRetryCommand extends BaseCommand {
     IntakeDeploySubsystem intakeDeploySubsystem;
+    public DoubleProperty timeout;
+    public double startTime = 0;
 
-    public IntakeDeployRetryCommand(IntakeDeploySubsystem intakeDeploy) {
+    public IntakeDeployRetryCommand(IntakeDeploySubsystem intakeDeploy, PropertyFactory pf) {
         this.intakeDeploySubsystem = intakeDeploy;
         addRequirements(intakeDeploy);
+        this.timeout = pf.createPersistentProperty("SecondsInTimeout", 3.0);
     }
+
+    public boolean isTimeoutExpired() {
+        //refer to WimpleWaitForMaintainerCommand
+        return Timer.getFPGATimestamp() > startTime + timeout.get();
+    }
+
 
     @Override
     public boolean isFinished() {
-        return intakeDeploySubsystem.intakeDeployIsExtended();
+        return (isTimeoutExpired()) || intakeDeploySubsystem.intakeDeployIsExtended();
     }
 }
