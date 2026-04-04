e47f0e99e6c730ae0adf34a11b3d062d92e21b06
Rongrui Zhu <ronrzhu@gmail.com>
2026-04-04 14:23:13 -0700

    Prepare shooter while navigating trench and fix rotate to hub

--- src/main/java/competition/command_groups/DriveFromNeutralZoneToAllianceAndShootCommandGroupFactory.java
+++ src/main/java/competition/command_groups/DriveFromNeutralZoneToAllianceAndShootCommandGroupFactory.java
@@ -5,7 +5,8 @@ import javax.inject.Provider;
 
 import competition.auto_programs.AimAndShootFromHereCommand;
 import competition.subsystems.collector_intake.commands.CollectorStopCommand;
-import competition.subsystems.drive.DriveSubsystem;
+import competition.subsystems.pose.TrajectoriesCalculation;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
@@ -14,6 +15,7 @@ public class DriveFromNeutralZoneToAllianceAndShootCommandGroupFactory {
     private final Provider<PrepareToShootCommandGroup> prepareToShootCommandProvider;
     private final Provider<CollectorStopCommand> collectorStopCommandProvider;
     private final Provider<AimAndShootFromHereCommand> aimAndShootFromHereCommandProvider;
+    private final Provider<PrepareToShootCommandGroup> prepareToShootCommandGroupProvider;
 
     @Inject
     public DriveFromNeutralZoneToAllianceAndShootCommandGroupFactory(
@@ -21,11 +23,12 @@ public class DriveFromNeutralZoneToAllianceAndShootCommandGroupFactory {
             Provider<PrepareToShootCommandGroup> prepareToShootCommandProvider,
             Provider<CollectorStopCommand> collectorStopCommandProvider,
             Provider<AimAndShootFromHereCommand> aimAndShootFromHereCommandProvider,
-            DriveSubsystem drive) {
+            Provider<PrepareToShootCommandGroup> prepareToShootCommandGroupProvider) {
         this.driveFromNeutralZoneToAllianceCommandProvider = driveFromNeutralZoneToAllianceCommandProvider;
         this.prepareToShootCommandProvider = prepareToShootCommandProvider;
         this.collectorStopCommandProvider = collectorStopCommandProvider;
         this.aimAndShootFromHereCommandProvider = aimAndShootFromHereCommandProvider;
+        this.prepareToShootCommandGroupProvider = prepareToShootCommandGroupProvider;
     }
 
     public SequentialCommandGroup create() {
@@ -35,10 +38,20 @@ public class DriveFromNeutralZoneToAllianceAndShootCommandGroupFactory {
         var driveFromNeutralToAlliance = new ParallelDeadlineGroup(
                 this.driveFromNeutralZoneToAllianceCommandProvider.get(),
                 this.collectorStopCommandProvider.get(),
-                this.prepareToShootCommandProvider.get());
-        group.addCommands(driveFromNeutralToAlliance);
-        group.addCommands(this.aimAndShootFromHereCommandProvider.get());
+                prepareToShootAtTrench()
+        ).withName("DriveFromNeutralToAllianceAndWarmUpShooter");
+        
+        // Getting rid of the middle step such that we can immediately fire after
+        // getting back to alliance from neutral zone.
+        // group.addCommands(this.getReadyForFiringCommandGroup.get());
 
+        group.addCommands(driveFromNeutralToAlliance, this.aimAndShootFromHereCommandProvider.get());
         return group;
+    }
+
+    public Command prepareToShootAtTrench() {
+        var command = prepareToShootCommandGroupProvider.get();
+        command.setPresetLocation(TrajectoriesCalculation.PresetShootingDistance.TRENCH);
+        return command;
     }
 }
