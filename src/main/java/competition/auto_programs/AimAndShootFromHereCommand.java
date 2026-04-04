7692107c5085815a1c407b51936af9602873302c
Alex Schokking <aschokking@gmail.com>
2026-04-04 12:36:07 -0700

    Slow close hood during firing

--- src/main/java/competition/auto_programs/AimAndShootFromHereCommand.java
+++ src/main/java/competition/auto_programs/AimAndShootFromHereCommand.java
@@ -4,6 +4,7 @@ import javax.inject.Inject;
 
 import competition.command_groups.ContinuousPrepareToShootFromHereCommand;
 import competition.command_groups.FireWhenShooterAndHoodReady;
+import competition.command_groups.IntakeSlowlyAndFireWhenReady;
 import competition.subsystems.drive.commands.RotateToHubCommand;
 import xbot.common.command.BaseParallelCommandGroup;
 
@@ -11,7 +12,7 @@ public class AimAndShootFromHereCommand extends BaseParallelCommandGroup {
 
     @Inject
     public AimAndShootFromHereCommand(ContinuousPrepareToShootFromHereCommand continuousPrepareToShootFromHereCommand,
-            FireWhenShooterAndHoodReady fireWhenShooterAndHoodReady,
+            IntakeSlowlyAndFireWhenReady fireWhenShooterAndHoodReady,
             RotateToHubCommand rotateToHub) {
 
         continuousPrepareToShootFromHereCommand.setTarget(ContinuousPrepareToShootFromHereCommand.ShootingTarget.HUB);
