5f82a987f249fa1626d26cacfe8933264911c3b6
AlanYuan99 <alanyuan709@gmail.com>
2026-04-04 22:21:38 -0700

    added oscillation to the command

--- src/main/java/competition/subsystems/intake_deploy/commands/IntakeDeploySmartRetractionCommand.java
+++ src/main/java/competition/subsystems/intake_deploy/commands/IntakeDeploySmartRetractionCommand.java
@@ -1,8 +1,10 @@
 package competition.subsystems.intake_deploy.commands;
 
 import competition.subsystems.intake_deploy.IntakeDeploySubsystem;
+import edu.wpi.first.units.measure.Angle;
 import xbot.common.command.BaseSetpointCommand;
 import xbot.common.controls.sensors.XTimer;
+import xbot.common.properties.AngleProperty;
 import xbot.common.properties.DoubleProperty;
 import xbot.common.properties.PropertyFactory;
 
@@ -13,41 +15,65 @@ import static edu.wpi.first.units.Units.Degrees;
 
 public class IntakeDeploySmartRetractionCommand extends BaseSetpointCommand {
     private final IntakeDeploySubsystem intakeDeploy;
-    private final DoubleProperty currentUpperThreshold;
-    private final DoubleProperty currentLowerThreshold;
-    private final DoubleProperty timeWaited;
+    private final DoubleProperty currentThreshold;
+    private final DoubleProperty oscillationDelayTime;
+
+    public final AngleProperty amplitude;
+    
+    public DoubleProperty period;
 
     private double stopTime = 0;
+    private boolean isReadyToOscillate = false;
+    private Angle oscillationTarget;
+    private double oscillationTime;
 
     @Inject
     public IntakeDeploySmartRetractionCommand(IntakeDeploySubsystem intakeDeploy, PropertyFactory propertyFactory) {
         super(intakeDeploy);
         this.intakeDeploy = intakeDeploy;
         propertyFactory.setPrefix(this);
-        this.currentUpperThreshold = propertyFactory.createPersistentProperty("Current Upper Threshold", 6);
-        this.currentLowerThreshold = propertyFactory.createPersistentProperty("Current Lower Threshold", 4);
-        this.timeWaited = propertyFactory.createPersistentProperty("Time Before Reattempt", 0.5);
+
+        this.currentThreshold = propertyFactory.createPersistentProperty("Current Threshold", 45);
+        this.oscillationDelayTime = propertyFactory.createPersistentProperty("Time Before Oscillation", 0.5);
+        this.amplitude = propertyFactory.createPersistentProperty("Amplitude", Degrees.of(7.5));
+        this.period = propertyFactory.createPersistentProperty("Time per cycle", 1);
     }
 
     @Override
     public void initialize() {
         log.info("Initialized IntakeDeploySmartRetraction");
+        intakeDeploy.setTargetValue(Degrees.of(intakeDeploy.retractedPosition.get()));
+    }
+
+    public Angle oscillate(double time) {
+        double runOscillation = amplitude.get().in(Degrees) * Math.sin(2 * Math.PI * time / period.get());
+        return Degrees.of(runOscillation);
     }
 
     @Override
     public void execute() {
-        double elapsedTimeFromStop = XTimer.getFPGATimestamp() - stopTime;
-        var currentIsAboveLowerThreshold = intakeDeploy.intakeDeployMotor.getCurrent().gt(Amps.of(currentLowerThreshold.get()));
-        var currentIsBelowUpperThreshold = intakeDeploy.intakeDeployMotor.getCurrent().lt(Amps.of(currentUpperThreshold.get()));
+        var currentIsAboveThreshold = intakeDeploy.intakeDeployMotor.getCurrent().gt(Amps.of(currentThreshold.get()));
 
-        if (currentIsAboveLowerThreshold && currentIsBelowUpperThreshold) {
-            intakeDeploy.setTargetValue(intakeDeploy.getCurrentValue()); //stops the intake
+        if (currentIsAboveThreshold && !isReadyToOscillate && stopTime == 0) {
+            intakeDeploy.setTargetValue(intakeDeploy.getCurrentValue()); // stops the intake
             stopTime = XTimer.getFPGATimestamp();
         }
 
-        if (elapsedTimeFromStop >= timeWaited.get()) { // have 500 milliseconds passed since stopping intake
-            stopTime = 0;
-            intakeDeploy.setTargetValue(Degrees.of(intakeDeploy.retractedPosition.get()));
+        if (stopTime > 0 && !isReadyToOscillate) {
+            double elapsedTimeFromStop = XTimer.getFPGATimestamp() - stopTime;
+
+            if (elapsedTimeFromStop >= oscillationDelayTime.get()) { // have 500 milliseconds passed since stopping intake
+                stopTime = 0;
+                isReadyToOscillate = true;
+                oscillationTarget = intakeDeploy.getCurrentValue();
+                oscillationTime = XTimer.getFPGATimestamp();
+            }
+        }
+        
+        if (isReadyToOscillate) {
+            double timeSinceOscillation = XTimer.getFPGATimestamp() - oscillationTime;
+            Angle target = oscillationTarget.plus(oscillate(timeSinceOscillation));
+            intakeDeploy.setTargetValue(target);
         }
     }
 
@@ -56,6 +82,7 @@ public class IntakeDeploySmartRetractionCommand extends BaseSetpointCommand {
 
     @Override
     public void end(boolean interrupted) {
+        super.end(interrupted);
         intakeDeploy.setTargetValue(Degrees.of(intakeDeploy.extendedPosition.get()));
     }
 
