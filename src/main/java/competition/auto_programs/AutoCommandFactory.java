dd681e937980ad3094a81d3b4969ec7bd0f28b84
Saqib Rokadia <saqib@saqibr.com>
2026-04-07 19:28:05 -0700

    Removing commented out code.

--- src/main/java/competition/auto_programs/AutoCommandFactory.java
+++ src/main/java/competition/auto_programs/AutoCommandFactory.java
@@ -108,9 +108,6 @@ public class AutoCommandFactory {
 
         group.addCommands(driveToNeutralZoneProvider.get());
 
-        // group.addCommands(new SequentialCommandGroup(intakeDeployExtendProvider.get())
-        //         .andThen(intakeDeploy.getWaitForAtGoalCommand()));
-
         group.addCommands(new ParallelDeadlineGroup(
                 driveAcrossMidNeutralZoneProvider.get(),
                 collectorIntakeProvider.get()));
