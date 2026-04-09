842d36e01ebc3c3b9b4badc3aa01c63bf7126dd6
Saqib Rokadia <saqib@saqibr.com>
2026-04-09 14:38:43 -0700

    Adding a wait for rotation when firiing. (#405)

--- src/main/java/competition/auto_programs/AimAndShootFromHereCommand.java
+++ src/main/java/competition/auto_programs/AimAndShootFromHereCommand.java
@@ -3,7 +3,7 @@ package competition.auto_programs;
 import javax.inject.Inject;
 
 import competition.command_groups.ContinuousPrepareToShootFromHereCommand;
-import competition.command_groups.IntakeSlowlyAndFireWhenReady;
+import competition.command_groups.IntakeSlowlyAndFireWhenReadyWithRotation;
 import competition.subsystems.drive.commands.RotateToHubCommand;
 import competition.subsystems.drive.commands.SwerveDriveWithJoysticksCommand;
 import xbot.common.command.BaseParallelCommandGroup;
@@ -12,12 +12,12 @@ public class AimAndShootFromHereCommand extends BaseParallelCommandGroup {
 
     @Inject
     public AimAndShootFromHereCommand(ContinuousPrepareToShootFromHereCommand continuousPrepareToShootFromHereCommand,
-                                      IntakeSlowlyAndFireWhenReady fireWhenShooterAndHoodReady,
+                                      IntakeSlowlyAndFireWhenReadyWithRotation fireWhenShooterAndHoodWithRotationReady,
                                       RotateToHubCommand rotateToHub, SwerveDriveWithJoysticksCommand swerve) {
 
         continuousPrepareToShootFromHereCommand.setTarget(ContinuousPrepareToShootFromHereCommand.ShootingTarget.HUB);
         continuousPrepareToShootFromHereCommand.setZeroHood(true);
 
-        this.addCommands(continuousPrepareToShootFromHereCommand, rotateToHub, swerve, fireWhenShooterAndHoodReady);
+        this.addCommands(continuousPrepareToShootFromHereCommand, rotateToHub, swerve, fireWhenShooterAndHoodWithRotationReady);
     }
 }
