257ad76862e5f4bfc2a9b2bac98439869bab1c3d
Alex Schokking <aschokking@gmail.com>
2026-04-10 13:58:17 -0700

    Remove unused HoodToZeroCommand import from OperatorCommandMap

--- src/main/java/competition/operator_interface/OperatorCommandMap.java
+++ src/main/java/competition/operator_interface/OperatorCommandMap.java
@@ -27,7 +27,6 @@ import competition.subsystems.drive.commands.RotateToHubCommand;
 import competition.subsystems.drive.commands.XPositionCommand;
 import competition.subsystems.hood.HoodSubsystem;
 import competition.subsystems.hood.commands.DropHoodForTrenchCommand;
-import competition.subsystems.hood.commands.HoodToZeroCommand;
 import competition.subsystems.hopper_roller.HopperRollerSubsystem;
 import competition.subsystems.intake_deploy.commands.IntakeDeployExtendCommand;
 import competition.subsystems.intake_deploy.commands.IntakeDeployOscillating;
@@ -98,8 +97,7 @@ public class OperatorCommandMap {
                                      HopperAndIntakeCommandGroup intakeCommand,
                                      HopperAndIntakeEjectCommandGroup ejectCommand,
                                      IntakeSlowlyAndFireWhenReady  intakeSlowlyAndFireWhenReady,
-                                     Provider<PrepareToShootCommandGroup> prepareToShootCommand,
-                                     Provider<HoodToZeroCommand> hoodToZeroCommandProvider) {
+                                     Provider<PrepareToShootCommandGroup> prepareToShootCommand) {
         var prepareToShootNear = prepareToShootCommand.get()
                 .setPresetLocation(TrajectoriesCalculation.PresetShootingDistance.NEAR);
         var prepareToShootTowerClose = prepareToShootCommand.get()
